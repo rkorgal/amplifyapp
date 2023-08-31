bd97aad4b4e20e5609c8ecbeb49df3c73e9d209f
Raghavendra <raghu.shetty@gmail.com>
2023-08-31 20:24:57 +0530

    Update App.js
    v2 version

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello world!!!</h1>
+        <h1>hello world V2!!!</h1>
       </header>
     </div>
   );
